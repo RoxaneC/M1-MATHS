b8da18f7f3656a07dcd5308bfc810e3e720a06f8
RoxaneC <61699049+RoxaneC@users.noreply.github.com>
2024-01-17 14:24:28 +0100

    Update analyse.cpp

--- Programmation_C++/TP1/analyse.cpp
+++ Programmation_C++/TP1/analyse.cpp
@@ -15,6 +15,21 @@ struct Fiche{
 int main(){
     ifstream fichier("smalldata.txt");
     vector<Fiche> vdata(2500);
+    
+    string prenom;
+    string ville;
+    int age;
+    double temps;
+
+    while(fichier >> prenom >> ville >> age >> temps;){
+        Fiche aux;
+        aux.prenom = prenom;
+        aux.ville = ville;
+        aux.age = age;
+        aux.temps = temps;
+
+        vdata.push_back(aux);
+    }
 
     fichier.close();
     return 0;
