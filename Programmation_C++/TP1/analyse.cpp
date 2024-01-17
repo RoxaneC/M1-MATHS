278a4f72809bcece3fd46668148c9e71f75a2fae
RoxaneC <61699049+RoxaneC@users.noreply.github.com>
2024-01-17 15:44:27 +0100

    Update analyse.cpp milieu question 3

--- Programmation_C++/TP1/analyse.cpp
+++ Programmation_C++/TP1/analyse.cpp
@@ -4,14 +4,13 @@
 #include <string>
 
 using namespace std;
-//
 struct Fiche{
     string prenom;
     string ville;
     int age;
     double temps;
 };
-//
+
 // question 3
 int question_3a(vector<Fiche> v){
     int count = 0;
@@ -45,6 +44,7 @@ bool question_3c(vector<Fiche> v){
     return count;
 }
 
+
 //
 int main(){
     ifstream fichier("smalldata.txt");
@@ -67,10 +67,34 @@ int main(){
     }
 
     // question 3
-    cout << "Nombre de personnes à Lyon : " << question_3a(vdata) << " (" << 100*count/2500 << "%)" << endl;
-    cout << "---> de moins de 30ans : " << question_3b(vdata) << endl;
-    cout << "Un Toulousain dont le prénom commance par \'A\' " << question_3c(vdata) << endl;
+    cout << "a) Nombre de personnes à Lyon (proportion) : " << question_3a(vdata) << " (" << 100*count/2500 << "%)" << endl;
+    cout << "b) ---> de moins de 30ans : " << question_3b(vdata) << endl;
+    cout << "c) Un Toulousain dont le prénom commance par \'A\' : " << question_3c(vdata) << endl;
 
+    int ind_min = 0;
+    int ind_max = 0;
+    for(int i=1; i<2500 ; i++){
+        if (v[i].age < v[ind_min].age){
+            ind_min=i;
+        } else if (v[i].age > v[ind_max].age){
+            ind_max=i;
+        }
+    }
+    cout << "d) Âge minimal : " << vdata[inf_min].age << " (" vdata[inf_min].prenom << ")" << endl;
+    cout << "   Âge maximal : " << vdata[ind_max].age << " (" vdata[ind_max].prenom << ")" << endl;
+
+    double moyenne=0;
+    double ecart_type=0;
+    for(int i=0; i<2500 ; i++){
+        moyenne += v[i].age;
+        ecart_type += v[i].age * v[i].age;
+    }
+    moyenne /= 2500;
+    ecart_type = ecart_type/2500 - moyenne*moyenne;
+    cout << "e) Âge moyen : " << moyenne << ", écart-type : " << ecart_type << endl;
+
+    //
+    
     fichier.close();
     return 0;
 }
