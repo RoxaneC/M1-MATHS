ae3a0e1146219e4a3742665502c285b88d55d990
RoxaneC <61699049+RoxaneC@users.noreply.github.com>
2024-01-17 15:07:40 +0100

    Update analyse.cpp début question 3

--- Programmation_C++/TP1/analyse.cpp
+++ Programmation_C++/TP1/analyse.cpp
@@ -1,27 +1,62 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <string>
 
 using namespace std;
-
+//
 struct Fiche{
     string prenom;
     string ville;
     int age;
     double temps;
 };
+//
+// question 3
+int question_3a(vector<Fiche> v){
+    int count = 0;
+    for(int i=0; i<2500 ; i++){
+        if (v[i].ville == "Lyon"){
+            count++;
+        }
+    }
+    return count;
+}
 
+int question_3b(vector<Fiche> v){
+    int count = 0;
+    for(int i=0; i<2500 ; i++){
+        if (v[i].ville == "Lyon") && (v[i].age < 30){
+            count++;
+        }
+    }
+    return count;
+}
 
+bool question_3c(vector<Fiche> v){
+    int count = 0;
+    bool existe;
+    for(int i=0; i<2500 ; i++){
+        if (v[i].ville == "Toulouse") && (v[i].prenom[0] == 'A'){
+            count++;
+        }
+    }
+    existe = (count > 0) ? true ; false;
+    return count;
+}
+
+//
 int main(){
     ifstream fichier("smalldata.txt");
-    vector<Fiche> vdata(2500);
-    
+    // vector<Fiche> vdata(2500);
+    vector<Fiche> vdata;
+
+    // question 2
     string prenom;
     string ville;
     int age;
     double temps;
-
-    while(fichier >> prenom >> ville >> age >> temps;){
+    while(fichier >> prenom >> ville >> age >> temps){
         Fiche aux;
         aux.prenom = prenom;
         aux.ville = ville;
@@ -30,6 +65,11 @@ int main(){
 
         vdata.push_back(aux);
     }
+
+    // question 3
+    cout << "Nombre de personnes à Lyon : " << question_3a(vdata) << " (" << 100*count/2500 << "%)" << endl;
+    cout << "---> de moins de 30ans : " << question_3b(vdata) << endl;
+    cout << "Un Toulousain dont le prénom commance par \'A\' " << question_3c(vdata) << endl;
 
     fichier.close();
     return 0;
