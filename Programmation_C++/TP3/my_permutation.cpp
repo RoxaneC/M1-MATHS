24895e099affb95fad0324cadaf761ba942ee523
RoxaneC <roxane.cellier@etu.sorbonne-universite.fr>
2024-02-12 11:01:29 +0100

    Avancement TP3 c++

--- Programmation_C++/TP3/my_permutation.cpp
+++ Programmation_C++/TP3/my_permutation.cpp
@@ -1,14 +1,50 @@
 #include "my_permutation.hpp"
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
 // Constructeurs
-Permutation::Permutation(int n0): n(n0), images(n0){
-	fiota(images.begin(), images.end(), 0);
+// question 2.4
+Permutation::Permutation(int _n): n(_n), images(_n){
+	for(int i=0 ; i<_n ; i++){
+		images[i] = i;
+	}
+}
+// question 2.11
+Permutation::Permutation(const vector<int> &v): n(v.size()), images(v) {
+	// suffit ?
 };
 
 // Méthodes
 
 
 // Opérateurs
-Permutation::operator <<()
+// question 2.5
+ostream & operator<<(ostream &o, const Permutation &P){
+	o << P.n << " : ";
+	for(int i=0 ; i<P.n ; i++) {
+		o << P.images[i] << " ";
+	}
+	o << "\n";
+	return o;
+};
+
+// question 2.7
+istream & operator>>(std::istream &i, Permutation &P){
+	//
+	string aux;
+	i >> P.n;
+	for(int j=0 ; j<P.n ; j++){
+		i >> P.images[j];
+	}
+	return i;
+};
+
+int main(){
+	// question 2.6
+	Permutation perm_test(6);
+	cout << perm_test;
+
+	return 0;
+}
--- Programmation_C++/TP3/my_permutation.hpp
+++ Programmation_C++/TP3/my_permutation.hpp
@@ -14,20 +14,26 @@ class Permutation{
 	// question 2.3
 	public:
 		// Constructeurs
-		Permutation(int n0=1);		// identité de taille n0
+		Permutation(int _n=1);		// identité de taille n0
 		Permutation(const std::vector<int> &v);		// création par vecteur
-		Permutation(int n0, std::mt19937 g);		// génération de taille n0 avec aléatoire g
+		Permutation(int _n, std::mt19937 g);		// génération de taille n0 avec aléatoire g
 		
 		// Méthodes
 		std::vector<int> fixed_points() const;
-		std::list<Cycle> cycles() const;
+		// std::list<Cycle> cycles() const;
 		int order() const;
 		Permutation inverse();
 		bool is_derangement() const;
 
 		// Opérateurs
-		friend ostream & operator<<(std::ostream &o, const Permutation &P);
+		friend std::ostream & operator<<(std::ostream &o, const Permutation &P);
+		friend std::istream & operator>>(std::istream &i, Permutation &P);
 		friend std::vector<int> operator*(const std::vector<int> &P1, const std::vector<int> &P2);
-		
+		// question 2.9
+		int operator[](int i) const {	return images[i]; };
+
+		// Accesseurs
+		// question 2.8
+		int size() const {	return n; };		
 };
 #endif
