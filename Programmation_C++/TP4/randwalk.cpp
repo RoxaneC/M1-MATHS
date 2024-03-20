fc54422fc6b4c8e0e2c585da32d5b5774accd970
RoxaneC <roxane.cellier@etu.sorbonne-universite.fr>
2024-03-21 00:17:17 +0100

    Fin TP4 C++

--- Programmation_C++/TP4/randwalk.cpp
+++ Programmation_C++/TP4/randwalk.cpp
@@ -4,6 +4,10 @@
 #include <ctime>
 #include <iostream>
 #include <fstream>
+#include <chrono>
+#include <algorithm>
+#include <deque>
+// #include <list>
 
 using namespace std;
 
@@ -32,11 +36,164 @@ void RandomWalk_with_Min::update(Alea &G){
     m = (val() < m) ? val() : m;
 };
 
+// question 4.6
+void measure_complexity_on_vector(long int N, mt19937 &G){
+    vector<double> c1(N);
+    vector<double> c2(N);
+    exponential_distribution<double> E(1);
+
+    auto t1 = chrono::system_clock::now();
+    for(int i=0 ; i<N ; i++)    c1[i] = E(G);
+    auto t2 = chrono::system_clock::now();
+    chrono::duration<double> tps = t2-t1;
+    cout << "Le remplissage de c1 dure : " << tps.count() << " seconde.\n";
+
+    t1 = chrono::system_clock::now();
+    int inf_10_1 = count_if(    c1.begin(), c1.end(),
+                                [](const double &d){   return d>10;} );
+    t2 = chrono::system_clock::now();
+    tps = t2-t1;
+    cout << "Le comptage des éléments >10 de c1 dure : " << tps.count() << " seconde.\n";
+
+    t1 = chrono::system_clock::now();
+    transform( c1.begin(), c1.begin()+100, c1.begin(),
+                []( double &d){    return d*d;} );
+    t2 = chrono::system_clock::now();
+    tps = t2-t1;
+    cout << "L'élévation au carré des 100 premier elem de c1 dure : " << tps.count() << " seconde.\n";
+
+    t1 = chrono::system_clock::now();
+    copy(c1.begin(), c1.end(), c2.begin());
+    t2 = chrono::system_clock::now();
+    tps = t2-t1;
+    cout << "La copie de c1 dans c2 dure : " << tps.count() << " seconde.\n";
+
+    t1 = chrono::system_clock::now();
+    sort(c1.begin(), c1.begin()+N/2);
+    t2 = chrono::system_clock::now();
+    tps = t2-t1;
+    cout << "Le tri de la moitié de c1 dure : " << tps.count() << " seconde.\n";
+
+    t1 = chrono::system_clock::now();
+    swap(c1, c2);
+    t2 = chrono::system_clock::now();
+    tps = t2-t1;
+    cout << "Le swap entre c1 et c2 dure : " << tps.count() << " seconde.\n";
+
+    t1 = chrono::system_clock::now();
+    ofstream file("chocolatine.cpp");
+    for(int i=0 ; i<c2.size() ; i++)    file << c2[i] << "\n";
+    file.close();
+    t2 = chrono::system_clock::now();
+    tps = t2-t1;
+    cout << "L'écriture de c2 dans un fichier dure : " << tps.count() << " seconde.\n";
+}
+
+// question 4.8
+void measure_complexity_on_deque(long int N, mt19937 &G){
+    deque<double> c1(N);
+    deque<double> c2(N);
+    exponential_distribution<double> E(1);
+
+    auto t1 = chrono::system_clock::now();
+    for(int i=0 ; i<N ; i++)    c1[i] = E(G);
+    auto t2 = chrono::system_clock::now();
+    chrono::duration<double> tps = t2-t1;
+    cout << "Le remplissage de c1 dure : " << tps.count() << " seconde.\n";
+
+    t1 = chrono::system_clock::now();
+    int inf_10_1 = count_if(    c1.begin(), c1.end(),
+                                [](const double &d){   return d>10;} );
+    t2 = chrono::system_clock::now();
+    tps = t2-t1;
+    cout << "Le comptage des éléments >10 de c1 dure : " << tps.count() << " seconde.\n";
+
+    t1 = chrono::system_clock::now();
+    transform( c1.begin(), c1.begin()+100, c1.begin(),
+                []( double &d){    return d*d;} );
+    t2 = chrono::system_clock::now();
+    tps = t2-t1;
+    cout << "L'élévation au carré des 100 premier elem de c1 dure : " << tps.count() << " seconde.\n";
+
+    t1 = chrono::system_clock::now();
+    copy(c1.begin(), c1.end(), c2.begin());
+    t2 = chrono::system_clock::now();
+    tps = t2-t1;
+    cout << "La copie de c1 dans c2 dure : " << tps.count() << " seconde.\n";
+
+    t1 = chrono::system_clock::now();
+    sort(c1.begin(), c1.begin()+N/2);
+    t2 = chrono::system_clock::now();
+    tps = t2-t1;
+    cout << "Le tri de la moitié de c1 dure : " << tps.count() << " seconde.\n";
+
+    t1 = chrono::system_clock::now();
+    swap(c1, c2);
+    t2 = chrono::system_clock::now();
+    tps = t2-t1;
+    cout << "Le swap entre c1 et c2 dure : " << tps.count() << " seconde.\n";
+
+    t1 = chrono::system_clock::now();
+    ofstream file("chocolatine.cpp");
+    for(int i=0 ; i<c2.size() ; i++)    file << c2[i] << "\n";
+    file.close();
+    t2 = chrono::system_clock::now();
+    tps = t2-t1;
+    cout << "L'écriture de c2 dans un fichier dure : " << tps.count() << " seconde.\n";
+}
+
+// question 4.9
+/* void measure_complexity_on_list(long int N, mt19937 &G){
+    list<double> c1(N);
+    list<double> c2(N);
+    exponential_distribution<double> E(1);
+
+    auto t1 = chrono::system_clock::now();
+    for(int i=0 ; i<N ; i++)    c1[i] = E(G);
+    auto t2 = chrono::system_clock::now();
+    chrono::duration<double> tps = t2-t1;
+    cout << "Le remplissage de c1 dure : " << tps.count() << " seconde.\n";
+
+    t1 = chrono::system_clock::now();
+    int inf_10_1 = count_if(    c1.begin(), c1.end(),
+                                [](const double &d){   return d>10;} );
+    t2 = chrono::system_clock::now();
+    tps = t2-t1;
+    cout << "Le comptage des éléments >10 de c1 dure : " << tps.count() << " seconde.\n";
+
+    t1 = chrono::system_clock::now();
+    transform( c1.begin(), c1.begin()+100, c1.begin(),
+                []( double &d){    return d*d;} );
+    t2 = chrono::system_clock::now();
+    tps = t2-t1;
+    cout << "L'élévation au carré des 100 premier elem de c1 dure : " << tps.count() << " seconde.\n";
+
+    t1 = chrono::system_clock::now();
+    copy(c1.begin(), c1.end(), c2.begin());
+    t2 = chrono::system_clock::now();
+    tps = t2-t1;
+    cout << "La copie de c1 dans c2 dure : " << tps.count() << " seconde.\n";
+
+    t1 = chrono::system_clock::now();
+    swap(c1, c2);
+    t2 = chrono::system_clock::now();
+    tps = t2-t1;
+    cout << "Le swap entre c1 et c2 dure : " << tps.count() << " seconde.\n";
+
+    t1 = chrono::system_clock::now();
+    ofstream file("chocolatine.cpp");
+    for(int i=0 ; i<c2.size() ; i++)    file << c2[i] << "\n";
+    file.close();
+    t2 = chrono::system_clock::now();
+    tps = t2-t1;
+    cout << "L'écriture de c2 dans un fichier dure : " << tps.count() << " seconde.\n";
+} */
+
 
 int main(){
     // question 4.2
     RandomWalk RW(0, 0.5);
-    mt19937 Gen(time(NULL));
+    mt19937 Gen(time(nullptr));
 
     cout << "S_" << RW.time() << " = " << RW.val() << "\n";
 
@@ -58,6 +215,31 @@ int main(){
     }
     file_rw.close();
     file_rw_min.close();
+
+    // question 4.7
+    cout << "\n\nQuestion 4.7 : Timer Avec std::vector\n";
+    int M = 10000000;
+    for(int i : {1,2,4,8}){
+        cout << "\tPour N = " << i*M << " : \n";
+        measure_complexity_on_vector(i*M, Gen);
+        cout << "\n";
+    }
+
+    // question 4.8
+    cout << "\n\nQuestion 4.8 : Timer Avec std::deque\n";
+    for(int i : {1,2,4,8}){
+        cout << "\tPour N = " << i*M << " : \n";
+        measure_complexity_on_deque(i*M, Gen);
+        cout << "\n";
+    }
+
+    // question 4.9
+    /* cout << "\n\nQuestion 4.8 : Timer Avec std::list\n";
+    for(int i : {1,2,4,8}){
+        cout << "\tPour N = " << i*M << " : \n";
+        measure_complexity_on_list(i*M, Gen);
+        cout << "\n";
+    } */
 
     return 0;
 }
--- Programmation_C++/TP4/texte_analyse.cpp
+++ Programmation_C++/TP4/texte_analyse.cpp
@@ -6,16 +6,29 @@
 #include <numeric>
 #include <set>
 #include <cmath>
+#include <tuple>
 
 using namespace std;
+using pair_su = pair<const string, unsigned>;
+
+tuple<double, unsigned /* , vector<string> */ > basic_statistics(const map<string,unsigned> &M){
+    int nb_mot = accumulate(	M.begin(), M.end(), 0,
+								[](int sum, const pair_su &p){ return sum+ p.second; } );
+    double moy_long = accumulate(	M.begin(), M.end(), 0.,
+									[nb_mot](double sum, const pair_su &p){ return sum+ double(p.first.size()*p.second)/nb_mot; } );
+
+    unsigned size_longest_word = max_element(  M.begin(), M.end(),
+                                                [](const pair_su &p1, const pair_su &p2){    return p1.first.size()<p2.first.size(); } )->first.size();
+
+    auto T=make_tuple(moy_long, size_longest_word);
+    return T;
+};
 
 int main(){
     // question 4.11
     ifstream file_decla("declaration.txt");
     
-    map<string, unsigned> S;
-    using pair_su = pair<const string, unsigned>;
-    
+    map<string, unsigned> S;    
     
     string word;
     while(file_decla >> word){
@@ -83,26 +96,47 @@ int main(){
 									[nb_mot](double sum, const pair_su &p){ return sum+ double(p.first.size()*p.second)/nb_mot; } );
 	cout << "7.0)\tLongueur moyenne des mots : " << moy_long << "\n";
 	double moy_long_carre = accumulate(	S.begin(), S.end(), 0.,
-										[nb_mot](double sum, const pair_su &p){ return sum+ double(pow(p.first.size()*p.second,2)) / nb_mot; } );
+										[nb_mot](double sum, const pair_su &p){ return sum+ double(p.first.size()*p.second*p.first.size()*p.second) / nb_mot; } );
 
     double moy_freq = accumulate(	S.begin(), S.end(), 0.,
 									[nb_mot](double sum, const pair_su &p){ return sum+ double(p.second)/nb_mot; } );
     double moy_freq_carre = accumulate(	S.begin(), S.end(), 0.,
-									[nb_mot](double sum, const pair_su &p){ return sum+ double(pow(p.second,2)) / nb_mot; } );
+									[nb_mot](double sum, const pair_su &p){ return sum+ double(p.second*p.second) / nb_mot; } );
     
     double moy_long_freq = accumulate(	S.begin(), S.end(), 0.,
 									[nb_mot](double sum, const pair_su &p){ return sum+ double(p.first.size()*p.second*p.second)/nb_mot; } );
     
-    double cov_long_freq = moy_long_freq - moy_long*moy_freq;    // cov(X,Y) = E[XY] - E[X]E[Y]
+    double cov_long_freq = moy_long_freq - moy_long*moy_freq;       // cov(X,Y) = E[XY] - E[X]E[Y]
 	cout << "7.0)\tCovarience entre longueur d'un mot et fréquence du mot : " << cov_long_freq << "\n";
 
-	double sdt_long = sqrt(moy_long_carre - pow(moy_long,2));	// ecart_type = sqrt(E[X^2] - E[X]^2)
-	double sdt_freq = sqrt(moy_freq_carre - pow(moy_freq,2)); 	// ecart_type = sqrt(E[X^2] - E[X]^2)
-	double corr_long_freq = cov_long_freq/(sdt_long*sdt_freq);	// corr = cov/std1*std2
+	double sdt_long = sqrt(moy_long_carre - moy_long*moy_long);     // ecart_type = sqrt(E[X^2] - E[X]^2)
+	double sdt_freq = sqrt(moy_freq_carre - moy_freq*moy_freq);     // ecart_type = sqrt(E[X^2] - E[X]^2)
+	double corr_long_freq = cov_long_freq/(sdt_long*sdt_freq);      // corr = cov/std1*std2
 	cout << "7)\tCorrélation entre longueur d'un mot et fréquence du mot : " << corr_long_freq << "\n";
+
+    // question 4.15
+    auto more_12_letters = [](const string &s){ return s.size()>=12; };
+    auto more_13_time = [](const int i){    return i>=13; };
+    auto no_letter_e = [](const string &s){ return !s.contains('e'); };     // c++23
+    auto last_letter_s = [](const string &s){   return s.back()!='s'; };
+
+    cout << "Question 4.15 :\t";
+    for(pair_su &elem : S){
+        if (more_12_letters(elem.first)
+                && more_13_time(elem.second)
+                && no_letter_e(elem.first)
+                && last_letter_s(elem.first))
+            cout << elem.first << ", ";
+    }
+    cout << "\n";
+
+    // question 4.18
+    double x; unsigned n;
+    auto T = basic_statistics(S);
+    tie(x,n) = T;
+    cout << "Question 4.18 :\tTuple ( long. moy. : " << x << " , size mot long : " << n << " )\n";
 
     return 0;
 }
 
-// 2 mots : constitution et organisation (Q15)
 // constitutionnellement
